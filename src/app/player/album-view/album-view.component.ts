b4b1d8905ba7d0eda12c05607748f85dbb2243b0
Matthias Frey <info@frey-it.com>
2019-09-28 00:17:56 +0200

    Album view

--- src/app/player/album-view/album-view.component.ts
+++ src/app/player/album-view/album-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { mergeMap, withLatestFrom, share } from 'rxjs/operators';
+import { mergeMap, withLatestFrom, share, tap, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { BsapiService } from 'src/app/shared/bsapi.service';
 import { ServiceSourceService } from '../service-source.service';
@@ -26,6 +26,17 @@ export class AlbumViewComponent implements OnInit {
     this.album$ = this.route.paramMap.pipe(
       withLatestFrom(this.service$),
       mergeMap(([params, service]) => this.bsapi.getSongsOfAlbum(params.get('albumid'), service)),
+      map(albumdata => {
+        // The album has no title or any metadata
+        // Get the title (it should be just one) from the album's songs
+        const albumtitlesFromSongs = albumdata.songs.map(song => song.alb).reduce((cur, next) => {
+          if (cur.indexOf(next)===-1) {
+            cur.push(next);
+          }
+          return cur;
+        }, []);
+        return {...albumdata, albumTitles: albumtitlesFromSongs};
+      }),
       share());
   }
 
