af2b0e5c33db7a7a039de4bdd415d4455f659776
Matthias Frey <info@frey-it.com>
2020-09-02 20:58:42 +0200

    add (settings): save/load settings to/from local storage

--- src/app/settings/settings.service.ts
+++ src/app/settings/settings.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { PlayerSettings } from '../shared/player-settings';
 import { BehaviorSubject } from 'rxjs';
 
-// TODO: initialise settings from (and save to) local storage
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +11,13 @@ export class SettingsService {
     ip: '192.168.0.102',
     port: 11000
   });
-  constructor() {}
+
+  constructor() {
+    const stored = this._getStored();
+    if (stored) {
+      this.setSettings(stored);
+    }
+  }
 
   getSettings$() {
     return this.state$;
@@ -20,5 +25,17 @@ export class SettingsService {
 
   setSettings(settings: PlayerSettings) {
     this.state$.next(settings);
+    this._setStored(settings);
+  }
+
+  private _getStored(): PlayerSettings {
+    const storage = window.localStorage;
+    const _stored = storage.getItem('settings');
+    return _stored ? {...JSON.parse(_stored)} as PlayerSettings : null;
+  }
+
+  private _setStored(settings: PlayerSettings) {
+    const storage = window.localStorage;
+    storage.setItem('settings', JSON.stringify(this.state$.getValue()));
   }
 }
